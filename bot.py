7b4a95669775bb63dce394618e363ed16db91617
Микита Маркевич <mykyta.markevych@gmail.com>
2019-03-06 00:20:46 +0200

    min change  bot

--- bot.py
+++ bot.py
@@ -10,6 +10,22 @@ from flask import Flask, request
 bot = telebot.TeleBot(token)
 server = Flask(__name__)
 
+@server.route('/' + token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://enigmatic-river-40567.herokuapp.com/' + token)
+    return "?", 200
+
+if __name__ == '__main__':
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+    utils.count_rows()
+    random.seed()
+    
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Щоб почати тестування, оберіть команду /test')
@@ -36,20 +52,6 @@ def check_answer(message):
             bot.send_message(message.chat.id, "Ні, правильна відповідь %s" %answer, reply_markup=keyboard_hider)
         utils.finish_user_game(message.chat.id)            
         
-@server.route('/' + token, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
 
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://enigmatic-river-40567.herokuapp.com/' + token)
-    return "?", 200
 
-if __name__ == '__main__':
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-    utils.count_rows()
-    random.seed()
-    
